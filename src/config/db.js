8f939286734c6eaa47cbd351fa02e2e9f95e168b
VMD <82048148+vihangamd@users.noreply.github.com>
2025-03-31 13:46:21 +0530

    Update db.js
    remove DB info

--- src/config/db.js
+++ src/config/db.js
@@ -11,7 +11,7 @@ const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   host: process.env.DB_HOST || "localhost",
   database: process.env.DB_DATABASE || "auto-service",
-  password: process.env.DB_PASSWORD || "12341",
+  password: process.env.DB_PASSWORD || "",
   port: process.env.DB_PORT || 5432,
 });
 
